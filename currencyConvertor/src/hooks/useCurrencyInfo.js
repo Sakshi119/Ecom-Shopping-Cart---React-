64248043b9401b081fe066d60903928c4d942856
Sakshi sen <71304358+Sakshi119@users.noreply.github.com>
2025-11-21 16:15:00 +0530

    Delete currencyConvertor directory

--- currencyConvertor/src/hooks/useCurrencyInfo.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-
-
-function useCurrencyInfo(currency){
-    useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json`)
-    },[])
-}
